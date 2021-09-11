e6c690c7f36109d6d25e40d5c0d5f895f3f34d7d
Alexandrress <alexandress12355@hotmail.com>
2021-09-11 18:25:01 +0200

    feat: added unit test

---
+++ Assets/Tests/CanCreateGameObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CanCreateGameObject
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CanCreateGameObjectSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            GameObject test = new GameObject("GameObject");
+            Assert.AreEqual("GameObject", test.name);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator CanCreateGameObjectWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
